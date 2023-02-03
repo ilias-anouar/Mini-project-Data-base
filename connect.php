58dc96984fb54cf967df82b59ec7d6615ed07e50
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-02-03 09:05:37 +0100

    add

--- connect.php
+++ connect.php
@@ -1,19 +1,16 @@
 <?php
-// Connect to MySQL
+require("connect.php");
 $servername = "localhost";
-$username = "username";
-$password = "ilias080701";
-$dbname = "advertisement";
+$username = "ilias";
+$password = "iilliiaass080701";
+$dbname = "ilias";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-
-// Close connection
-mysqli_close($conn);
 ?>
