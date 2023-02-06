de74aef4d39ba074d3967658fc0d9d9d7128cbd3
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-02-06 15:31:16 +0100

    add

--- search.php
+++ search.php
@@ -1,4 +1,16 @@
-<?php 
+<?php
+require "connect.php";
+if (isset($_GET["search"])) {
+    if ($_GET["type"]!= null) {
+        $type = $_GET["type"];
+    }
+    if ($_GET["type"] != null) {
+        $max = $_GET["Price-max"];
+    }
+    if ($_GET["type"] != null) {
+        $min = $_GET["Price-min"];
+    }
 
+}
 
 ?>
