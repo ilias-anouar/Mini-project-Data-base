aed4b6dda704ca39452220886ddf10588df03c49
yasmine <daifaney@gmail.com>
2023-02-06 15:25:11 +0000

    add

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "announcements";
+$dbname = "yasmine";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
--- search.php
+++ search.php
@@ -12,4 +12,39 @@ if (isset($_GET["search"])) {
     }
 }
 
+function component(){
+$element= "
+ <div class=\"m-4\">
+   <div class=\"card \" style=\"width: 18rem;\">
+ 
+
+       <div class=\"card-image\">
+           <img class=\"card-img-top\" src=\"https://via.placeholder.com/150x100\"alt=\"Card image cap\">
+
+       <div
+            class=\"image-overlay d-flex flex-column gap-2 justify-content-center align-items-center\">
+            <button>Edit</button>
+
+            <button data-bs-toggle=\"modal\" data-bs-target=\"#deletemodal\">Delete</button>
+            <button>Details</button>
+        </div>
+    </div>
+    <div class=\"card-body\">
+        <h5 class=\"card-title\">Card title</h5>
+
+        <p class=\"card-text\">Some quick example text.</p>
+    </div>
+</div>
+";
+    echo $element;
+}
+
+
+
+
+
+
+
+
+
 ?>
