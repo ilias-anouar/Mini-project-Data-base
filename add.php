81558f03e7fdb4e52a079729219b4e5a8c5334c4
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-02-06 16:26:17 +0100

    add

--- add.php
+++ add.php
@@ -1,31 +1,38 @@
 <?php
 require "connect.php";
 
-if (isset($_GET["add"])) {
-    echo "hi";
+if (!empty($_GET["title"])) {
+    $title = $_GET["title"];
+}
+if (!empty($_GET["Address"])) {
+    $Address = $_GET["Address"];
+}
+if (!empty($_GET["Space"])) {
+    $Space = $_GET["Space"];
+}
+if (!empty($_GET["date"])) {
+    $date = $_GET["date"];
+}
+if (!empty($_GET["price"])) {
+    $price = $_GET["price"];
+}
+if (!empty($_GET["type"])) {
+    $type = $_GET["type"];
+}
+if (!empty($_GET["Description"])) {
+    $Description = $_GET["Description"];
+}
+if (!empty($_GET["image"])) {
+    $image = $_GET["image"];
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $sql = "INSERT INTO advertisement  (`ID`, `title`, `image`, `description`, `Area`, `address`, `amount`, `Date`, `type`)
-VALUES (NUll, 'Doe', 'image', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error soluta recusandae eligendi natus libero expedita totam neque, impedit illo! Omnis aut pariatur possimus ab id iusto. Delectus, obcaecati expedita','700','address addressaddressaddressaddress','12000000',now(),'selling' )";
+VALUES (null,'$title','$image','$Description','$Space','$Address','$price','$date','$type')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->connect_error;
 }
 $conn->close();
 ?>
