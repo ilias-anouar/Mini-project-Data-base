6a098343fe4f136e9357a551477c65ac4c0586a6
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-03-01 15:21:39 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,26 +1,5 @@
 <?php
-// require "connect.php";
-// if (isset($_GET['id']) ){
-
-//     $ID = $_GET['id'];
-
-
-//     //store a request to the db to delete the card by its id
-//     $Delete = "DELETE FROM advertisement WHERE `advertisement`.`ID` = $ID" ;
-
-//     //execute the request above
-//     $Result = $conn->query($Delete)->execute();
-
-//     // if result had an error print it out 
-//     if (!$Result) {
-//         echo "Error: " . $Delete . "<br>" . $conn->connect_error;
-//     }
-
-//     // return to the main page wich is index.php
-//     header("location: index.php");
-// } 
 include "connect.php";
-// include "index.php";
 if (isset($_POST['ID'])) {
     $id = $_POST['ID'];
     $Delete = "DELETE FROM advertisement WHERE `advertisement`.`ID` = $id";
